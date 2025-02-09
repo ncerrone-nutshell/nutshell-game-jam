2a7a17b7cf6e89a68d7f5129d3ee5f0c18f625b6
Drew White <dwhite@nutshell.com>
2025-02-09 09:13:34 -0500

    Messing around with post processing

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,8 @@
+import {
+    BrightnessContrast,
+    EffectComposer,
+    Vignette,
+} from '@react-three/postprocessing';
 import { NutCamera } from './components/camera/nut-camera';
 import { OfficeSpace } from './components/environment/office-space';
 
@@ -5,6 +10,10 @@ export function App() {
     return (
         <>
             <NutCamera />
+            <EffectComposer>
+                <Vignette eskil={false} offset={0.05} darkness={0.9} />
+                <BrightnessContrast brightness={0.05} contrast={0.2} />
+            </EffectComposer>
             {/* <OrbitControls /> */}
             {/* TODO: Toggle on and off active lights as a random event */}
             <OfficeSpace activeLights={true} />
